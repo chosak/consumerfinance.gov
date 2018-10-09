5b8a691602852e1201ce70667d0b3d8ebad1e7e9
Andy Chosak <andy.chosak@cfpb.gov>
2018-10-09 11:48:18 -0400

    run slow tests on travis

--- travis_run.sh
+++ travis_run.sh
@@ -11,9 +11,7 @@ if [ "$RUNTEST" == "frontend" ]; then
     npm run gulp test -- --travis --headless
     bash <(curl -s https://codecov.io/bash) -F frontend -X coveragepy
 elif [ "$RUNTEST" == "backend" ]; then
-    tox -e lint
-    tox -e missing-migrations
-    TEST_DATABASE_URL=postgres://travis:travis@localhost:5433/travis tox -e fast
+    tox
     bash <(curl -s https://codecov.io/bash) -F backend
 elif [ "$RUNTEST" == "docs" ]; then
     mkdocs build
