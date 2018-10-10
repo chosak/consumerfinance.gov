9dd0002914ce9646fd0e99b61017ecae0ca3087f
Andy Chosak <andy.chosak@cfpb.gov>
2018-10-10 10:25:12 -0400

    run django tests in parallel (travis only)
    also temporarily disable travis branch testing to be able to build
    against non-pr branches.

--- travis_run.sh
+++ travis_run.sh
@@ -13,6 +13,7 @@ if [ "$RUNTEST" == "frontend" ]; then
 elif [ "$RUNTEST" == "backend" ]; then
     tox -e lint
     tox -e missing-migrations
+    export DJANGO_TEST_PROCESSES=`python -c 'import multiprocessing; print(multiprocessing.cpu_count())'`
     TEST_DATABASE_URL=postgres://travis:travis@localhost:5433/travis tox -e fast
     bash <(curl -s https://codecov.io/bash) -F backend
 elif [ "$RUNTEST" == "docs" ]; then
